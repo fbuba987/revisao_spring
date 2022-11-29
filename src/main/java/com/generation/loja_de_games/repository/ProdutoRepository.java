ba3a2fdf979bbcedbf139d040d94814eb1000205
“fbuba987” <115090534+fbuba987@users.noreply.github.com>
2022-11-29 11:56:34 -0300

    repository

--- src/main/java/com/generation/loja_de_games/repository/ProdutoRepository.java
+++ src/main/java/com/generation/loja_de_games/repository/ProdutoRepository.java
@@ -4,9 +4,11 @@ package com.generation.loja_de_games.repository;
 import com.generation.loja_de_games.model.Produtos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produtos, Long> {
 
     public List<Produtos> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
