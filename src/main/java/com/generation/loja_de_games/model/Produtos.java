5ff2fdad73259e096f6346a47525ff515e434a68
“fbuba987” <115090534+fbuba987@users.noreply.github.com>
2022-12-05 15:30:45 -0300

    Emplimentando o security

--- src/main/java/com/generation/loja_de_games/model/Produtos.java
+++ src/main/java/com/generation/loja_de_games/model/Produtos.java
@@ -30,6 +30,10 @@ public class Produtos {
     @JsonIgnoreProperties("produtos")
     private Categoria categoria;
 
+    @ManyToOne
+    @JsonIgnoreProperties("produtos")
+    private Usuario usuario;
+
     public Long getId() {
         return id;
     }
--- src/main/java/com/generation/loja_de_games/repository/ProdutoRepository.java
+++ src/main/java/com/generation/loja_de_games/repository/ProdutoRepository.java
@@ -3,6 +3,7 @@ package com.generation.loja_de_games.repository;
 
 import com.generation.loja_de_games.model.Produtos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +15,7 @@ public interface ProdutoRepository extends JpaRepository<Produtos, Long> {
 
     public List<Produtos> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 
+    // @Query("select e from Produtos e where e.preco < :preco")
     public List<Produtos> findAllByPrecoGreaterThanOrderBOrderByPreco(BigDecimal preco);
     public List<Produtos> findAllByPrecoLessThanOrderByPrecoDesc(BigDecimal preco);
 }
